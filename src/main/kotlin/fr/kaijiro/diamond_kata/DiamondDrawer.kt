86f2e4b6435a3cb9cd60df846000bde46ee831a2
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-05-07 16:28:40 +0200

    Initial commit

---
+++ src/main/kotlin/fr/kaijiro/diamond_kata/DiamondDrawer.kt
@@ -0,0 +1,8 @@
+package fr.kaijiro.diamond_kata
+
+
+class DiamondDrawer {
+    fun drawForLetter(character: Char): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ src/test/kotlin/fr/kaijiro/diamond_kata/DiamondTest.kt
@@ -0,0 +1,13 @@
+package fr.kaijiro.diamond_kata
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+class DiamondTest {
+
+    @Test
+    internal fun `test env should be running`() {
+        assertThat(true).isTrue()
+    }
+}
