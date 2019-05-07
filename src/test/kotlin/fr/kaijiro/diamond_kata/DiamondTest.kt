d8252e4e574945f54e73b7b41fcec9446d6ad9a3
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-05-07 17:18:08 +0200

    Refactor the second case

--- src/test/kotlin/fr/kaijiro/diamond_kata/DiamondTest.kt
+++ src/test/kotlin/fr/kaijiro/diamond_kata/DiamondTest.kt
@@ -11,14 +11,18 @@ class DiamondTest {
     @Test
     internal fun `diamond for letter A should be just a single letter A`() {
         val diamondDrawer = DiamondDrawer()
+
         val diamondDrawn = diamondDrawer.drawForLetter('A')
+
         assertThat(diamondDrawn).isEqualTo("A")
     }
 
     @Test
     internal fun `diamond for letter B should be a diamond with A and B letters`() {
         val diamondDrawer = DiamondDrawer()
+
         val diamondDrawn = diamondDrawer.drawForLetter('B')
+
         val diamondExpected = File("src/test/resources/diamondB.txt").readText(Charset.defaultCharset())
         assertThat(diamondDrawn).isEqualTo(diamondExpected)
     }
