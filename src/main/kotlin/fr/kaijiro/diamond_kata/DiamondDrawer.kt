93a0c7b44f9c34db3aa514a256fc8e2a7c557852
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-05-07 17:06:40 +0200

    Second case : Build a diamond with the letter B

--- src/main/kotlin/fr/kaijiro/diamond_kata/DiamondDrawer.kt
+++ src/main/kotlin/fr/kaijiro/diamond_kata/DiamondDrawer.kt
@@ -1,8 +1,15 @@
 package fr.kaijiro.diamond_kata
 
-
 class DiamondDrawer {
     fun drawForLetter(character: Char): String {
+        if(character == 'B'){
+            return buildString {
+                appendln(" A")
+                appendln("B B")
+                append(" A")
+            }
+        }
+
         return "A"
     }
 }
--- src/test/kotlin/fr/kaijiro/diamond_kata/DiamondTest.kt
+++ src/test/kotlin/fr/kaijiro/diamond_kata/DiamondTest.kt
@@ -2,6 +2,8 @@ package fr.kaijiro.diamond_kata
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.charset.Charset
 
 
 class DiamondTest {
@@ -13,5 +15,11 @@ class DiamondTest {
         assertThat(diamondDrawn).isEqualTo("A")
     }
 
-
+    @Test
+    internal fun `diamond for letter B should be a diamond with A and B letters`() {
+        val diamondDrawer = DiamondDrawer()
+        val diamondDrawn = diamondDrawer.drawForLetter('B')
+        val diamondExpected = File("src/test/resources/diamondB.txt").readText(Charset.defaultCharset())
+        assertThat(diamondDrawn).isEqualTo(diamondExpected)
+    }
 }
