864335cc32433a051a88a9cff4b7edbe66bd1418
Tristan Lasalle <tristan.lasalle@gmail.com>
2019-05-20 01:14:16 +0200

    Refactor -> First step : Highlight the pattern in order to find an algorithm

--- src/main/kotlin/fr/kaijiro/diamond_kata/Diamond.kt
+++ src/main/kotlin/fr/kaijiro/diamond_kata/Diamond.kt
@@ -5,35 +5,88 @@ class Diamond private constructor(private val character: Char) {
     override fun toString(): String {
         if(this.character == 'D'){
             return buildString {
-                appendln("   A")
-                appendln("  B B")
-                appendln(" C   C")
-                appendln("D     D")
-                appendln(" C   C")
-                appendln("  B B")
-                append("   A")
+                append(addSpaces(3))
+                appendln("A")
+
+                append(addSpaces(2))
+                append("B")
+                append(addSpaces(1))
+                appendln("B")
+
+                append(addSpaces(1))
+                append("C")
+                append(addSpaces(3))
+                appendln("C")
+
+                append("D")
+                append(addSpaces(5))
+                appendln("D")
+
+                append(addSpaces(1))
+                append("C")
+                append(addSpaces(3))
+                appendln("C")
+
+                append(addSpaces(2))
+                append("B")
+                append(addSpaces(1))
+                appendln("B")
+
+                append(addSpaces(3))
+                append("A")
             }
         }
 
         if(this.character == 'C'){
             return buildString {
-                appendln("  A")
-                appendln(" B B")
-                appendln("C   C")
-                appendln(" B B")
-                append("  A")
+                append(addSpaces(2))
+                appendln("A")
+
+                append(addSpaces(1))
+                append("B")
+                append(addSpaces(1))
+                appendln("B")
+
+                append("C")
+                append(addSpaces(3))
+                appendln("C")
+
+                append(addSpaces(1))
+                append("B")
+                append(addSpaces(1))
+                appendln("B")
+
+                append(addSpaces(2))
+                append("A")
             }
         }
 
         if(this.character == 'B'){
             return buildString {
-                appendln(" A")
-                appendln("B B")
-                append(" A")
+                append(addSpaces(1))
+                appendln("A")
+
+                append("B")
+                append(addSpaces(1))
+                appendln("B")
+
+                append(addSpaces(1))
+                append("A")
             }
         }
 
-        return "A"
+        return buildString {
+            append("A")
+        }
+    }
+
+    private fun addSpaces(spacesNumber: Int): String {
+        var spacesString = ""
+        for(i in 0 until spacesNumber){
+            spacesString += " "
+        }
+
+        return spacesString
     }
 
 
