fc9cd9eb6ea5b78d3fafcdb4c470452e699747c4
Tristan Lasalle <tristan.lasalle@gmail.com>
2019-05-21 22:22:25 +0200

    Refactor -> Second step : Implement a first version of the algorithm

--- src/main/kotlin/fr/kaijiro/diamond_kata/Diamond.kt
+++ src/main/kotlin/fr/kaijiro/diamond_kata/Diamond.kt
@@ -3,80 +3,36 @@ package fr.kaijiro.diamond_kata
 class Diamond private constructor(private val character: Char) {
 
     override fun toString(): String {
-        if(this.character == 'D'){
-            return buildString {
-                append(addSpaces(3))
-                appendln("A")
-
-                append(addSpaces(2))
-                append("B")
-                append(addSpaces(1))
-                appendln("B")
-
-                append(addSpaces(1))
-                append("C")
-                append(addSpaces(3))
-                appendln("C")
-
-                append("D")
-                append(addSpaces(5))
-                appendln("D")
-
-                append(addSpaces(1))
-                append("C")
-                append(addSpaces(3))
-                appendln("C")
-
-                append(addSpaces(2))
-                append("B")
-                append(addSpaces(1))
-                appendln("B")
-
-                append(addSpaces(3))
-                append("A")
-            }
-        }
-
-        if(this.character == 'C'){
-            return buildString {
-                append(addSpaces(2))
-                appendln("A")
-
-                append(addSpaces(1))
-                append("B")
-                append(addSpaces(1))
-                appendln("B")
-
-                append("C")
-                append(addSpaces(3))
-                appendln("C")
-
-                append(addSpaces(1))
-                append("B")
-                append(addSpaces(1))
-                appendln("B")
-
-                append(addSpaces(2))
-                append("A")
-            }
-        }
-
-        if(this.character == 'B'){
-            return buildString {
-                append(addSpaces(1))
-                appendln("A")
-
-                append("B")
-                append(addSpaces(1))
-                appendln("B")
-
-                append(addSpaces(1))
-                append("A")
-            }
+        if(character == 'A'){
+            return "A"
         }
 
         return buildString {
-            append("A")
+            for(characterCode in 'A'.toInt()..character.toInt()){
+                val offset = character.toInt() - characterCode
+                append(addSpaces(offset))
+
+                if(characterCode == 'A'.toInt()){
+                    appendln('A')
+                } else {
+                    append(characterCode.toChar())
+                    append(addSpaces(-1 + 2 * (characterCode - 'A'.toInt())))
+                    appendln(characterCode.toChar())
+                }
+            }
+
+            for(characterCode in (character.toInt() - 1) downTo 'A'.toInt()){
+                val offset = character.toInt() - characterCode
+                append(addSpaces(offset))
+
+                if(characterCode == 'A'.toInt()){
+                    append('A')
+                } else {
+                    append(characterCode.toChar())
+                    append(addSpaces(-1 + 2 * (characterCode - 'A'.toInt())))
+                    appendln(characterCode.toChar())
+                }
+            }
         }
     }
 
